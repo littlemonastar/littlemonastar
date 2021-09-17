36f20763272f7f722e4b322c7ee39d97c3989563
Mona <42650066+littlemonastar@users.noreply.github.com>
2021-09-16 22:56:05 -0700

    update links

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,16 @@ export default function Home() {
           </a>
         </h1>
 
-        <h2>"✌"</h2>
+        <h2>"✌"
+
+        <FacebookShareButton
+          url={'https://github.com/next-share'}
+          quote={'next-share is a social share buttons for your next React apps.'}
+          hashtag={'#nextshare'}
+        >
+            <FacebookIcon size={32} round />
+            </FacebookShareButton>
+        </h2>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
         <a
